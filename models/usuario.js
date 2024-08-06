f9f7f6bd5faaa10206c37fa53e68de2bfaed131b
Jorge Tomé <jorge.tomejr@outlook.com>
2024-08-06 08:13:25 -0400

    criação da camada models usuario e contato

---
+++ models/usuario.js
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+export const Usuario = model("usuario", new Schema({
+    nome:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String,
+    }
+}));
